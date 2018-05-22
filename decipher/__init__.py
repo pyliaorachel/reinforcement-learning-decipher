650b3ae28c5be8edc8834cf7ae340592556308f8
Peiyu Liao <u3512485@connect.hku.hk>
2018-05-22 16:32:09 +0800

    Add hint caesar cipher multilevel difficulty

--- decipher/__init__.py
+++ decipher/__init__.py
@@ -8,4 +8,23 @@ register(
 register(
     id='HintCaesarCipher-v0',
     entry_point='decipher.envs:HintCaesarEnv',
+    kwargs={ 'base': 10, 'simple_hint': True }
 )
+
+register(
+    id='HintCaesarCipher-v1',
+    entry_point='decipher.envs:HintCaesarEnv',
+    kwargs={ 'base': 10 }
+)
+
+register(
+    id='HintCaesarCipher-v2',
+    entry_point='decipher.envs:HintCaesarEnv',
+    kwargs={ 'simple_hint': True }
+)
+
+register(
+    id='HintCaesarCipher-v3',
+    entry_point='decipher.envs:HintCaesarEnv',
+)
+
